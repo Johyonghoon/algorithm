83581d345b6d8a7a3ffcddfad68a748631641df1
johyonghoon <johyonghoon01@gmail.com>
2023-05-25 11:35:53 +0900

    dynamic programming :: 2579 solved

--- dynamic_programming/2579_계단_오르기.py
+++ dynamic_programming/2579_계단_오르기.py
@@ -1,14 +1,26 @@
 # 2579번 계단 오르기
 # https://www.acmicpc.net/problem/2579
 
-# not solved
-
 n = int(input())
-arr = []
-d = [0 for _ in range(n)]
+arr = [0]
 for _ in range(n):
-    arr.append([int(input()), 1])
-print(arr)
-for i in range(3, n):
-    arr[i] += max(arr[i-1], arr[i-2])
-print(arr[n-1])
+    arr.append(int(input()))
+if n == 1:
+    print(arr[1])
+elif n == 2:
+    print(arr[1] + arr[2])
+else:
+    d = [0 for _ in range(n + 1)]
+    d[1:3] = [arr[1], arr[1]+arr[2]]
+
+    for i in range(3, n+1):
+        if d[i] != 0:
+            continue
+        d[i] = max(d[i-3] + arr[i-1], d[i-2]) + arr[i]
+    print(d[n])
+
+"""
+10 20 15 25 10 20
+
+
+"""
