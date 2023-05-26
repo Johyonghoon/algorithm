8b5a22476d5eb2853010ac3e89edca4337ec4e89
johyonghoon <johyonghoon01@gmail.com>
2023-05-26 15:45:46 +0900

    binary search :: 2805 solved:

--- binary_search/2805_나무_자르기.py
+++ binary_search/2805_나무_자르기.py
@@ -1,26 +1,26 @@
 # 2805번 나무 자르기
 # https://www.acmicpc.net/problem/2805
 
-# not solved
 import sys
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-total, start, end = 0, min(arr), max(arr)
 
-while start <= end:
-    h = (start + end) // 2
-    print(start, end, h)
-    for i in arr:
-        if i > h:
-            total += i - h
-    if total == m:
-        print(h)
-        break
-    elif total > m:
-        start = h + 1
-    else:
-        end = h - 1
-    total = 0
+def binary_search(arr, target, start, end):
+    result = 0
+    while start <= end:
+        mid = (start + end) // 2
+        total = 0
+        for i in arr:
+            if i > mid:
+                total += i - mid
+        if total < target:
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+    return result
+
+print(binary_search(arr, m, 0, max(arr)))
