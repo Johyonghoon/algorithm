ec394c87600f550becd0340b0549a9dd0ec140ca
johyonghoon <johyonghoon01@gmail.com>
2023-06-08 15:36:05 +0900

    ~ chapter 9 done

---
+++ 9_etc/0902_eratos.py
@@ -0,0 +1,20 @@
+import math
+
+
+def eratos(n):
+    array = [True for _ in range(n+1)]
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if array[i] == True:
+            j = 2
+            while i * j <= n:
+                array[i * j] = False
+                j += 1
+
+    for i in range(2, n+1):
+        if array[i]:
+            print(i, end=' ')
+
+
+if __name__ == '__main__':
+    n = int(input())
+    eratos(n)
